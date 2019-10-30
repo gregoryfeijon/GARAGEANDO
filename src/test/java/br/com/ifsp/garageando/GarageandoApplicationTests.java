6b2e49f58249ee699814d6c00e4e4659599648e0
Gregory Feijon <gregory.feijon@outlook.com>
2019-10-30 20:55:38 -0300

    FIRST COMMIT

---
+++ src/test/java/br/com/ifsp/garageando/GarageandoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsp.garageando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
