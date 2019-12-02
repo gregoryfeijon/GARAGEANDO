2a65a7ae26703770759438efcb11ae832f26e024
Gregory Feijon <gregory.feijon@outlook.com>
2019-12-01 21:14:51 -0300

    [CREATE] Adição do EventoDTOBuilder.

--- src/test/java/br/com/ifsp/garageando/model/EventoDTOBuilder.java
+++ src/test/java/br/com/ifsp/garageando/model/EventoDTOBuilder.java
@@ -1,10 +1,49 @@
 package br.com.ifsp.garageando.model;
 
+import java.util.List;
+
+import br.com.ifsp.garageando.api.dto.EventoDTO;
+import br.com.ifsp.garageando.api.dto.WsDTO;
+
 /**
- *	1 de dez de 2019
+ * 1 de dez de 2019
  *
- *	@author gregory.feijon
+ * @author gregory.feijon
  */
-public class EventoDTOBuilder {
-	
+public class EventoDTOBuilder<T> extends WsDTO<T> {
+
+	private EventoDTO<T> eventoDTO = new EventoDTO<>();
+
+	public void novo() {
+		this.eventoDTO = new EventoDTO<>();
+	}
+
+	public EventoDTO<T> build() {
+		return eventoDTO;
+	}
+
+	public EventoDTOBuilder<T> withId(Long id) {
+		eventoDTO.setId(id);
+		return this;
+	}
+
+	public EventoDTOBuilder<T> withUsuario(Usuario usuario) {
+		eventoDTO.setUsuario(usuario);
+		return this;
+	}
+
+	public EventoDTOBuilder<T> withEndereco(Endereco endereco) {
+		eventoDTO.setEndereco(endereco);
+		return this;
+	}
+
+	public EventoDTOBuilder<T> withEvento(T evento) {
+		eventoDTO.setEntity(evento);
+		return this;
+	}
+
+	public EventoDTOBuilder<T> withLocais(List<T> locais) {
+		eventoDTO.setEntities(locais);
+		return this;
+	}
 }
